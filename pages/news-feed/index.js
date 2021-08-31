bc4c284888fd2f7cf8725d272b26951c0af45ac1
anuradha-gurusinghe <anuradha.gurusinghe@gmail.com>
2021-08-31 22:36:48 +0530

    image added

---
+++ pages/news-feed/index.js
@@ -0,0 +1,37 @@
+import Navigation from "../../components/Navigation/Navigation";
+import classes from "./index.module.css";
+import Image from "next/image";
+import HeroMainImage from "../../assets/Hero/HeroMainImage.png";
+
+
+function Newsfeed() {
+  return (
+    <div>
+      {/* <Navigation /> */}
+      <div className={classes.mainCard}>
+        <div className={classes.topContainer}>
+          <div className={classes.userImageContainer}>
+              
+          </div>
+          <div className={classes.userDetailsContainer}>
+            <p className={classes.userName}>username</p>
+            <p>Lorem Ipsum</p>
+            <p>
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+              erat, sed diam voluptua.
+            </p>
+          </div>
+        
+        </div>
+        <div classname={classes.postContainer}>
+        <span>
+            <Image src={HeroMainImage} alt="HeroMainImage" />
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Newsfeed;
